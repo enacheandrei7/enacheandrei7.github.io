cda0354bc99b87cd0b551e56fe9016280bd5a5aa
enacheandrei7 <enache.andrei.george@gmail.com>
2021-11-13 20:33:23 +0200

    Added Angulat project

---
+++ src/app/components/description-plane/description-plane.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-description-plane',
+  templateUrl: './description-plane.component.html',
+  styleUrls: ['./description-plane.component.css']
+})
+export class DescriptionPlaneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
