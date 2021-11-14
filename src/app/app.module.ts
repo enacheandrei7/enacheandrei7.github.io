3be7bee1e00c2255376278d652665dc7620b1361
enacheandrei7 <enache.andrei.george@gmail.com>
2021-11-14 17:14:54 +0200

    Finished the design step of the Landing Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DescriptionPlaneComponent } from './components/description-plane/description-plane.component';
+import { TeamComponent } from './components/team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DescriptionPlaneComponent
+    DescriptionPlaneComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
